255b6438788a0a8c1e346885927d67359ae67d9c
Karolinny Guimarães <karolinny.silva@gmail.com>
2020-04-28 00:18:58 -0300

    Update

--- challenge.js
+++ challenge.js
@@ -1,17 +1,12 @@
 var geraEndereco = document.querySelector(".botao-endereco");
 
-
 geraEndereco.addEventListener("click", function(){
 
-    coletaDados();
-
-    var insereTexto = document.createElement("label");
-    var criaBr = document.createElement("br");
-    insereTexto.innerHTML = "alou";
-
-    var PrimeiroModuloh2 = document.querySelector(".primeiro-modulo");
-    PrimeiroModuloh2.appendChild(criaBr);
-    PrimeiroModuloh2.appendChild(insereTexto);
+    endereco = coletaDados();
+   
+    ZeraDiv();
+    
+    GeraElemento(endereco);
 
 });
 
@@ -19,18 +14,30 @@ geraEndereco.addEventListener("click", function(){
 function coletaDados(){
     var obtemDados = document.querySelectorAll(".campo");
 
-    var array = obtemDados;
+      var array = obtemDados;
 
-    var endereco = "O usuário mora em 3 / 4, no bairro 2, na rua 0 com nº 2."
+    var endereco = 'O usuário mora em 3 / 4, no bairro 2, na rua 0 com nº 1.'
 
     obtemDados.forEach(function(dados, i){
         var itemDados = dados.value;
-        
-        console.log(i.toString(), itemDados);
-        endereco.replace(i, itemDados);
-        console.log(endereco);
-
-        console.log(mudaTextoX);
+        endereco = endereco.replace(i, itemDados);
     })
-    
+
+    return endereco;
+}
+
+
+function ZeraDiv(){
+    document.querySelector(".resposta").innerHTML = '';
+}
+
+function GeraElemento(endereco){
+    var insereTexto = document.createElement("label");
+    insereTexto.classList.add('endereco')
+    var criaBr = document.createElement("br");
+    insereTexto.innerHTML = endereco;
+
+    var PrimeiroModuloh2 = document.querySelector(".resposta");
+    PrimeiroModuloh2.appendChild(criaBr);
+    PrimeiroModuloh2.appendChild(insereTexto);
 }
